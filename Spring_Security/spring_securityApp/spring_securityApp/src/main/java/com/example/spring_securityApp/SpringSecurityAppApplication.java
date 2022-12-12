63e9989de2d084adee041777ebaf3be8a3be899c
Bush96 <92211699+Bush96@users.noreply.github.com>
2022-12-12 16:40:49 +0300

    Add files via upload

---
+++ Spring_Security/spring_securityApp/spring_securityApp/src/main/java/com/example/spring_securityApp/SpringSecurityAppApplication.java
@@ -0,0 +1,17 @@
+package com.example.spring_securityApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAppApplication.class, args);
+	}
+
+}
+
+
+//не хочет запускаться, видимо какие то дела с зависимостями
+//т к проект в такой структуре преподносился в 2019 году
---
+++ Spring_Security/spring_securityApp/spring_securityApp/src/main/java/com/example/spring_securityApp/security/PersonDetails.java
@@ -0,0 +1,58 @@
+package com.example.spring_securityApp.security;
+
+import com.example.spring_securityApp.models.Person;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+
+
+//когда работаем  секьюрити принято использовать класс обертку на сущностях
+public class PersonDetails implements UserDetails {
+    private final Person person;
+
+    public PersonDetails(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.person.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.person.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    // Нужно, чтобы получать данные аутентифицированного пользователя
+    public Person getPerson() {
+        return this.person;
+    }
+}
